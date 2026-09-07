ef0b03b6c70c5fcb533aa8ba4d97841acc74e2cf
kimadzn <kimadzn@gmail.com>
2026-09-07 12:19:57 -0300

    Via Vep

---
+++ ViaCep/ConsumerViaCep/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Digite o CEP: ");
+        string cep = Console.ReadLine();
+
+        string enderecoUrl = $"https://viacep.com.br/ws/{cep}/json/";
+
+        using (HttpClient cliente = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage resposta = await cliente.GetAsync(enderecoUrl);
+
+                if (resposta.IsSuccessStatusCode)
+                {
+                    string json = await resposta.Content.ReadAsStringAsync();
+
+                    Endereco endereco = JsonSerializer.Deserialize<Endereco>(json);
+
+                    Console.WriteLine("\nEndereço encontrado:");
+                    Console.WriteLine($"CEP: {endereco.Cep}");
+                    Console.WriteLine($"Logradouro: {endereco.Logradouro}");
+                    Console.WriteLine($"Complemento: {endereco.Complemento}");
+                    Console.WriteLine($"Bairro: {endereco.Bairro}");
+                    Console.WriteLine($"Cidade: {endereco.Localidade}");
+                    Console.WriteLine($"Estado: {endereco.Uf}");
+                    Console.WriteLine($"DDD: {endereco.Ddd}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro ao consultar o CEP.");
+                }
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Erro: {erro.Message}");
+            }
+        }
+    }
+}
